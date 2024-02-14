aece4d53a08ce7fc0860805658f3d6c6144bbc53
Piper Thunstrom <pathunstrom@gmail.com>
2024-02-14 13:45:28 -0800

    Build from project directory

--- src/coulomb/main.py
+++ src/coulomb/main.py
@@ -3,10 +3,9 @@ import pathlib
 
 def build(build_dir: pathlib.Path, source_dir: pathlib.Path):
     """"""
-
-    index = '<!DOCTYPE html><html lang="en"><head><title>My Cool Site</title></head><body><h1>Hello World!</h1></body>'
-    with open(build_dir / "index.html", "w") as output_file:
-        output_file.write(index)
+    for file_path in source_dir.glob("**/*.html"):
+        output_path = build_dir/file_path.relative_to(source_dir)
+        output_path.write_bytes(file_path.read_bytes())
 
 
 def run():  # Not testable due to: output dir is real.
@@ -16,4 +15,5 @@ def run():  # Not testable due to: output dir is real.
     output_path = pathlib.Path.cwd() / build_dir / site_name
     output_path.mkdir(parents=True, exist_ok=True)
 
-    build(output_path)
+    source_path = pathlib.Path.cwd() / "project"
+    build(output_path, source_path)
--- tests/test_build.py
+++ tests/test_build.py
@@ -6,8 +6,14 @@ import coulomb
 def test_build(tmp_path: pathlib.Path):
     output_dir = tmp_path / "my-site"  # Pretend this comes from configuration
     output_dir.mkdir(parents=True, exist_ok=True)  # Must exist for the builder.
+    input_dir = tmp_path / "project"
+    input_dir.mkdir(parents=True, exist_ok=True)
+    good_path = input_dir / "index.html"
+    bad_path = input_dir / "project.py"
+    good_path.touch()
+    bad_path.touch()
 
-    coulomb.main.build(output_dir)
+    coulomb.main.build(output_dir, input_dir)
 
     expected_file = output_dir / "index.html"
     assert expected_file.exists()
